8aa75c3a23983739fbd8cf29510c666b79310bd8
oscartcastillo <49007213+oscartcastillo@users.noreply.github.com>
2024-06-15 00:53:56 -0600

    commit

--- anim.js
+++ anim.js
@@ -2,37 +2,24 @@ var audio = document.querySelector("audio");
 var lyrics = document.querySelector("#lyrics");
 
 var lyricsData = [
-    { text: "Cuando me siento en tus brazos, se me olvida todo", time: 2 },
+    { text: "Cuando me siento en tus brazos, se me olvida todo", time: 0 },
     { text: "Soy afortunado por tenerte a ti", time: 6 },
     { text: "Aquí, despertando a mi lado", time: 13 },
-    { text: "Estoy en deuda porque llegaste a mi vida", time: 14 },
-    { text: "Como si estuvieras hecha justo a mi medida", time: 22 },
-    { text: "Y yo no lo pedí, solito me llegó", time: 27 },
-    { text: "Y vale el doble, sí, un regalo de Dios", time: 32 },
-    { text: "Destinado para mí", time: 38 },
-    { text: "Que nada te falte", time: 42 },
-    //{ text: "Voy a ser el encargado de cuidarte siempre", time: 42 },
-    //{ text: "Mi única misión será hacerte muy feliz", time: 44 },
-    //{ text: "En las buenas y en las malas estaré contigo", time: 46 },
-    //{ text: "Para lo que necesites", time: 48 },
-    //{ text: "Pase lo que pase, voy a estar ahí", time: 50 },
-    //{ text: "La recompensa para mí", time: 52 },
-    //{ text: "Es mirar tu linda carita sonreír", time: 54 },
-    //{ text: "¡Ay-ay-ay, chiquita!", time: 56 },
-    //{ text: "Es Julión Álvarez", time: 58 },
-    //{ text: "Y su Norteño Banda, oiga, ¡ah!", time: 60 },
-    //{ text: "Como si estuvieras hecha justo a mi medida", time: 62 },
-    //{ text: "Y yo no lo pedí, solito me llegó", time: 64 },
-    //{ text: "Y vale doble, sí, un regalo de Dios", time:  },
-    //{ text: "Destinado para mí", time: 8 },
-    //{ text: "Voy a ser el encargado de cuidarte siempre", time: 8 },
-    //{ text: "Que nada te falte", time: 8 },
-    //{ text: "Mi única misión será hacerte muy feliz", time: 8 },
-    //{ text: "En las buenas y en las malas estaré contigo", time: 8 },
-    //{ text: "Para lo que necesites", time: 8 },
-    //{ text: "Pase lo que pase, voy a estar ahí", time: 8 },
-    //{ text: "La recompensa para mí", time: 8 },
-    //{ text: "Es mirar tu linda carita sonreír", time: 8 }
+    { text: "Estoy en deuda porque llegaste a mi vida", time: 19 },
+    { text: "Como si estuvieras hecha justo a mi medida", time: 27 },
+    { text: "Y yo no lo pedí, solito me llegó", time: 33 },
+    { text: "Y vale el doble, sí, un regalo de Dios", time: 38 },
+    { text: "Destinado para mí", time: 46 },
+    //
+    { text: "Voy a ser el encargado de cuidarte siempre", time: 52 },
+    { text: "Que nada te falte", time: 59 },
+    { text: "Mi única misión será hacerte muy feliz", time: 66 },
+    
+    { text: "En las buenas y en las malas estaré contigo", time: 74 },
+    { text: "Para lo que necesites", time: 82 },
+    { text: "Pase lo que pase, voy a estar ahí", time: 84 },
+    { text: "La recompensa para mí", time: 94 },
+    { text: "Es mirar tu linda carita sonreír", time: 98 },
 ];
 
 function updateLyrics() {
